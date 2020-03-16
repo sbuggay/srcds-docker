bdca60b84fafad718f88b2db340e3d4c6cf64336
sbuggay <sbuggay@gmail.com>
2020-03-16 01:29:45 -0700

    Add hostname injection

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+
+# Inject hostname if it's not already in the autoexec
+if [ ! grep -q hostname ../cfg/autoexec.cfg ];then
+    SERVER_HOSTNAME="${SERVER_HOSTNAME:-Counter-Strike: Global Offensive Dedicated Server}"
+    echo not found
+fi
+
 csgo/srcds_run -game csgo -autoupdate -steam_dir $SERVER -steamcmd_script $SERVER/update.txt $@
