250e6e7bbf55f414fac8c4c642d0c1fe60463c50
sbuggay <sbuggay@gmail.com>
2020-03-15 22:08:19 -0700

    Fix plugin script

--- scripts/install-plugins.sh
+++ scripts/install-plugins.sh
@@ -3,7 +3,7 @@ cd $HOME
 
 installPlugin () {
     wget -qO- $1 -O tempplugin.zip
-    unzip -o tempplugin.zip -d $SERVER/csgo/csgo/
+    unzip -o tempplugin.zip -d $2
     rm tempplugin.zip
 }
 
@@ -15,10 +15,10 @@ disablePlugin () {
     mv $SERVER/csgo/csgo/addons/sourcemod/plugins/$1 $SERVER/csgo/csgo/addons/sourcemod/plugins/disabled/
 }
 
-installPlugin https://github.com/splewis/csgo-pug-setup/releases/download/2.0.5/pugsetup_2.0.5.zip
-installPlugin https://github.com/splewis/csgo-practice-mode/releases/download/1.3.3/practicemode_1.3.3.zip
-installPlugin https://github.com/splewis/csgo-multi-1v1/releases/download/1.1.9/multi1v1_1.1.9.zip
-installPlugin https://github.com/Maxximou5/csgo-deathmatch/raw/master/deathmatch.zip
+installPlugin https://github.com/splewis/csgo-pug-setup/releases/download/2.0.5/pugsetup_2.0.5.zip $SERVER/csgo/csgo/
+installPlugin https://github.com/splewis/csgo-practice-mode/releases/download/1.3.3/practicemode_1.3.3.zip $SERVER/csgo/csgo/
+installPlugin https://github.com/splewis/csgo-multi-1v1/releases/download/1.1.9/multi1v1_1.1.9.zip $SERVER/csgo/csgo/
+installPlugin https://github.com/Maxximou5/csgo-deathmatch/raw/master/deathmatch.zip $SERVER/csgo/csgo/addons/sourcemod/
 
 disablePlugin pugsetup.smx
 disablePlugin practicemode.smx
